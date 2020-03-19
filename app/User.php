dc3b49caa684a1c8018424566dc3109b15e8928e
nick <nghhoese@avans.nl>
2020-03-19 19:49:42 +0100

    encryptie fixie

--- app/User.php
+++ app/User.php
@@ -1,48 +1,15 @@
 <?php
 
 namespace App;
-
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+
 class User extends Authenticatable
 {
     use Notifiable;
-
-    public function roles()
-    {
-        return $this->belongsToMany('App\Role');
-    }
-
-    /**
-     * @param string|array $roles
-     */
-    public function authorizeRoles($roles)
-    {
-        if (is_array($roles)) {
-            return $this->hasAnyRole($roles) ||
-                abort(401, 'This action is unauthorized.');
-        }
-        return $this->hasRole($roles) ||
-            abort(401, 'This action is unauthorized.');
-    }
-    /**
-     * Check multiple roles
-     * @param array $roles
-     */
-    public function hasAnyRole($roles)
-    {
-        return null !== $this->roles()->whereIn('name', $roles)->first();
-    }
-    /**
-     * Check one role
-     * @param string $role
-     */
-    public function hasRole($role)
-    {
-        return null !== $this->roles()->where('name', $role)->first();
-    }
 
     /**
      * The attributes that are mass assignable.
@@ -70,4 +37,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function setEmailAttribute($value)
+    {
+        $this->attributes['email'] = Crypt::encryptString($value);
+    }
+
+    public function getEmailAttribute($value)
+    {
+        return Crypt::decryptString($value);
+    }
 }
